0db8db282c19b31500ab9d29eb5b905741b026dd
acejang1994 <acejang1994@gmail.com>
2015-09-10 22:34:42 -0400

    chapter 1 exercise

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h> 
+
+int count = 0;
+
+void get_card(char *card_name) {
+	puts("Enter the card_name: "); 
+	scanf("%2s", card_name);	
+}
+
+int counter(int val){
+	if ((val > 2) && (val < 7)) {
+		count++;
+	} else if (val == 10) {
+		count--; 
+	}
+	return count;
+}
+
+int set_value(char card_name[]) {
+	int val;
+	switch(card_name[0]) {
+	case 'K':
+	case 'Q':
+	case 'J':
+		val = 10;
+	    break;
+	case 'A':
+		val = 11;
+		break;
+	default:
+		val = atoi(card_name);
+		if ((val < 1) || (val > 10)) {
+			puts("I don't understand that value!");
+		}
+	}
+	return val;
+}
+
 int main()
 {
 	char card_name[3];
-	int count = 0;
+	int val;
 	while ( card_name[0] != 'X') {
-		puts("Enter the card_name: "); scanf("%2s", card_name);
-		int val = 0; 
-		switch(card_name[0]) {
-	        case 'K':
-	        case 'Q':
-	        case 'J':
-				val = 10;
-	            break;
-	        case 'A':
-				val = 11;
-				break;
-			case 'X':
-				continue;
-			default:
-			val = atoi(card_name);
-				if ((val < 1) || (val > 10)) {
-					puts("I don't understand that value!");
-					continue;
-				}
-		}
-		if ((val > 2) && (val < 7)) {
-			count++;
-		} else if (val == 10) {
-			count--; 
-		}
-		printf("Current count: %i\n", count); 
+		get_card(card_name); 
+		val = set_value(card_name);
+		counter(val);
+		printf("Current count: %i\n", count);
 	}
 	return 0; 
 }
